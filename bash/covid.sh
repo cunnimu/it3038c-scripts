d629b14cfb3ea820eb36bf2ac7be8d09845c0470
cunnimu <cunnimu@mail.uc.edu>
2023-09-09 16:17:30 -0400

    Lab 2

--- bash/covid.sh
+++ bash/covid.sh
@@ -7,7 +7,11 @@ POSITIVE=$(echo $DATA | jq '.[0].positive')
 NEGATIVE=$(echo $DATA | jq '.[0].negative')
 DEATH=$(echo $DATA | jq '.[0].death')
 HOSPITALIZED=$(echo $DATA | jq '.[0].hospitalized')
+POSINC=$(echo $DATA | jq '.[0].positiveIncrease')
+NEGINC=$(echo $DATA | jq '.[0].negativeIncrease')
+HOSINC=$(echo $DATA | jq '.[0].hospitalizedIncrease')
 TODAY=$(date)
 
 echo "On $TODAY, there were $POSITIVE positive COVID cases, $NEGATIVE negative tests, $DEATH deaths, and $HOSPITALIZED hospitalized."
+echo "This is an increase of $POSINC positive tests, $NEGINC negative tests, and $HOSINC hospitalized."
 
