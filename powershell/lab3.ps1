e44be5258f6a5bf73bb72fd282ecc958220409bc
cunnimu <143312142+cunnimu@users.noreply.github.com>
2023-09-16 02:18:49 -0400

    Add files via upload

---
+++ powershell/lab3.ps1
@@ -0,0 +1,16 @@
+﻿function getIP{
+
+    (Get-NetIPAddress).IPv4Address | Select-String "192*"
+
+}
+
+
+
+$IP = getIP
+$VERSION = $host.Version | Select-Object -ExpandProperty "Major"
+$DATE = Get-Date -Format "dddd, MMMM dd, yyyy"
+
+$BODY = "This machine's IP is $IP. User is $env:USERNAME. Hostname is $env:COMPUTERNAME. Powershell version $VERSION. Today's Date is $Date."
+
+write-host($BODY)
+$BODY | Out-File C:\Lab3.txt
