469faf1489072dcc2b71d47be1fe0f2f61fca71b
cunnimu <143312142+cunnimu@users.noreply.github.com>
2023-09-21 18:31:29 -0400

    Lab3

---
+++ powershell/Lab3.ps1
@@ -0,0 +1,18 @@
+﻿function getIP{
+
+    (Get-NetIPAddress).IPv4Address | Select-String "192*"
+
+}
+
+
+
+$IP = getIP
+$VERSION = $host.Version | Select-Object -ExpandProperty "Major"
+$USER = $env:USERNAME
+$HOSTNAME = $env:COMPUTERNAME
+$DATE = Get-Date -Format "dddd, MMMM dd, yyyy"
+
+$BODY = "This machine's IP is $IP. User is $USER. Hostname is $HOSTNAME. Powershell version $VERSION. Today's Date is $DATE."
+
+write-host($BODY)
+$BODY | Out-File C:\Lab3.txt
