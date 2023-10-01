416aad97cba5e5c8ecfe87f130c6e1e3ea41772d
cunnimu <143312142+cunnimu@users.noreply.github.com>
2023-10-01 18:48:36 -0400

    Added Lab 5

---
+++ labs/Lab5.py
@@ -0,0 +1,22 @@
+'''
+This is a simple number guessing game. 
+The script will generate a number between 1 and 10.
+Then you guess. It will tell if it's too high or too low.
+'''
+
+import random
+
+num = random.randint(1,10)
+guess = ''
+
+print('I am thinking of a number.')
+while guess != num:
+    #print('Guess: ')
+    guess = int(input('Guess: '))
+
+    if guess == num:
+        print('You guessed it! The number was ' + str(num))
+    elif guess < num:
+        print('Too low. Try again!')
+    elif guess > num:
+        print('Too high. Try again!')
