dbd5c4b8e192057961faf89abed2d9fbbad82236
cunnimu <143312142+cunnimu@users.noreply.github.com>
2023-10-27 17:19:58 -0400

    Update Lab8.py

--- labs/Lab8/Lab8.py
+++ labs/Lab8/Lab8.py
@@ -1,5 +1,12 @@
 # Simple look at the top 5 most active stocks from Yahoo!Finance
 
+# Output will look something like this:
+# Name: Ford Motor Company  Price: 9.96  Change: -1.39  % Change: -12.25%
+# Name: Amazon.com, Inc.  Price: 127.74  Change: +8.17  % Change: +6.83%
+# Name: Tesla, Inc.  Price: 207.30  Change: +1.54  % Change: +0.75%
+# Name: Intel Corporation  Price: 35.54  Change: +3.02  % Change: +9.29%
+# Name: Bank of America Corporation  Price: 25.17  Change: -0.95  % Change: -3.64%
+
 import requests
 from bs4 import BeautifulSoup
 
